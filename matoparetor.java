7529659d4834f407958526a23bffdda53956b663
Furkan Selim Özkan <furkanselimozkan@ogr.eskisehir.edu.tr>
2024-10-25 23:58:14 +0300

    Add files via upload

---
+++ matoparetor.java
@@ -0,0 +1,49 @@
+public class matoparetor {
+    public static void main(String[] args) {
+        /*
+        + ---> Toplama Operatörü
+        - ---> Çıkarma Operatörü
+        / ---> Bölme Operatörü
+        * ---> Çarpma Operatörü
+        % ---> Kalan Operatörü
+
+        */
+        /*
+        System.out.println(3 + 4.2);
+        System.out.println(3 - 5f);  float bır deger olarak cıkacak karsımıza
+        System.out.println(10 / 4.0);   10/4 yaparsan ınt deger gelır double deger ıcın ıkısınde bırını double olarak yaz
+        System.out.println(3 * 4.5);
+        System.out.println(10 % 4);  kalan operatoru 10 un 4 ıle bolumunden kalan verır
+        */
+       // System.out.println((3f / 4) + (4 * 5));
+
+        //arttırma ve azaltma operatorlerı
+
+
+       /* int a =4;
+        a=a+2;   // amam bu uzu yol kısa halı a +=2;
+        System.out.println(a);
+        int b =6;
+        b=b*3;      //b*=4;
+        System.out.println(b);
+
+        */
+/*a+=1;  //var normalde ama bunu d kısa yolu var  a++;
+b-=1; //bunun da kıa yolu b--;
+System.out.println(a);
+System.out.println(b);
+aralarınd abır fark var sunlar arasında    ++a ve --b
+int a =7;
+System.out.println(a++);  bu sekılde deger gunellenmeden yazılıyoe ekrana
+System.out.println(++a);
+deger dırek guncellenerke yazılıyor
+
+
+
+  bırde bunların ıslem sırası var bunun ıcınde parantez kullan klasık ıslem oncelıgı
+
+
+ */
+        System.out.println(3f/4+4*5);  //boyle yapacagına parantez at gec
+    }
+}
