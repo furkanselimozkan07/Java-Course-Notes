9801c6328ea7449394ca3e871ab4bd31db09eab5
Furkan Selim Özkan <furkanselimozkan@ogr.eskisehir.edu.tr>
2024-10-28 23:22:06 +0300

    Add files via upload

---
+++ KosulluDurumlar2.java
@@ -0,0 +1,149 @@
+
+// import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class KosulluDurumlar2 {
+    public static void main(String[] args) {
+
+        /*
+        if (kosul) {
+
+
+        }
+        else if (baska bir kosul) {
+//verılen ıslemı bırden fazla kosul ıle kontrol etmen gerektıgınde kullanrısn
+        }
+
+        else if (baska bir kosul) {
+
+        }
+
+                //
+                //
+        else {
+
+
+        }
+
+//burda elseıf den farkı ıf blogu yukardakılerın saglanıp saglanmadıgına bakmz sadece kendı sartını saglıyorsa dırek calsıtırı ondan burda 95 gırsen hepsı calsıırı yanı
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lütfen notunuzu giriniz:");
+        int not = scanner.nextInt();
+
+        if (not >= 90) {
+            System.out.println("AA");
+
+        }
+        if (not >=  85) {
+            System.out.println("BA");
+
+        }
+        if (not >=  80) {
+            System.out.println("BB");
+
+        }
+        if (not >=  75) {
+            System.out.println("CB");
+
+        }
+        if (not >=  70) {
+            System.out.println("CC");
+
+        }
+        if (not >=  65) {
+            System.out.println("DC");
+
+        }
+        if (not >=  60) {
+            System.out.println("DD");
+
+        }
+
+        else {
+            System.out.println("Dersten Kaldınız...");
+
+
+        }
+
+
+    }
+}
+ */
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Lutfen ıslem gırın:  ");
+        int islem = scanner.nextInt();
+        if (islem == 1) {
+            System.out.println("1. islem");
+
+        }
+        else if (islem == 2) {
+            System.out.println("2. islem");
+        }
+        else if (islem == 3) {
+            System.out.println("3. islem");
+        }
+        else if (islem == 4) {
+            System.out.println("4. islem");
+        }
+        else{
+            System.out.println("Gecerlı ıslem gırın ");
+        }
+
+
+
+
+
+
+
+    }}
+
+
+//bu dogru olan sekıl
+/*Scanner scanner = new Scanner(System.in);
+        System.out.println("Lütfen notunuzu giriniz:");
+int not = scanner.nextInt();
+
+        if (not >= 90) {
+        System.out.println("AA");
+
+        }
+               else if (not >=  85) {
+        System.out.println("BA");
+
+        }
+             else   if (not >=  80) {
+        System.out.println("BB");
+
+        }
+               else if (not >=  75) {
+        System.out.println("CB");
+
+        }
+               else if (not >=  70) {
+        System.out.println("CC");
+
+        }
+               else if (not >=  65) {
+        System.out.println("DC");
+
+        }
+               else if (not >=  60) {
+        System.out.println("DD");
+
+        }
+
+                else {
+                System.out.println("Dersten Kaldınız...");
+
+
+        }
+
+
+                }
+                }
+
+ */
