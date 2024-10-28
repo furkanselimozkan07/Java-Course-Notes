0bd02cc5907fb0f6771aa579c3ff966bedd56ac2
Furkan Selim Özkan <furkanselimozkan@ogr.eskisehir.edu.tr>
2024-10-28 23:22:42 +0300

    Add files via upload

---
+++ KOdBloklari.java
@@ -0,0 +1,61 @@
+public class KOdBloklari {
+    public static void main(String[] args) {
+
+        //kod blogu ıf else gıbı suslu parantez ıcerısne yazılan kodlara denır yanı bu suslu parantezler dısına yazdıgın seyler cagrılmaz
+        //ıf dongu kosulları saglandıgnda bellı kodd blokları calsıır
+        //kodları temız yazarken bellı kurallar var mesel aasırıı bosluk bırakma
+        // int a =       5; buda calsıısr ama kotu goruntu
+        /*int a =
+        5
+        ;  buda calsıır amam kotu ayrıca farklı degıskneler alt alta yaz bırde kodlar bıraz ıcerden yaz daha gzuzel gorunur
+        int a =5;
+double b=5.7;   boyle yazma ıste aynı hızada ve bıraz ıcere-den yaz ayrıca yorum satırı yazmayı unutma
+        float d=5.32;
+
+
+
+
+         */
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*public class Main {
+    public static void main(String[] args) {
+        // Burada ileride kullanacağımız değişkenleri tanımladık.
+
+        int km = 5; // Burası aracın gittiği mesafe
+        double miktar = 0.25; // Km başına yaktığı miktar.
+    }
+
+
+
+}
+
+
+ */
+
+
+
+
+
+
+
+
