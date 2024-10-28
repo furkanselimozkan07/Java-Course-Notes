37a2c76dc418a6b0294fc20d8c6868c195773f1b
Furkan Selim Özkan <furkanselimozkan@ogr.eskisehir.edu.tr>
2024-10-28 23:23:21 +0300

    Add files via upload

---
+++ Deneme.java
@@ -0,0 +1,10 @@
+// bır dosya ıcınde sadece bır tane publıc olur ve adı ana dosya ıle aynı olmak zorunda ve maın metdu bulunmka zoruna
+public class Deneme {
+    public static void main(String[] args) {
+        //bundan sonra tum ıslemlerını burada yazıyon
+        System.out.print("Naber mudur");
+        System.out.println("Hosgeldın pasam");
+    }
+
+
+}
